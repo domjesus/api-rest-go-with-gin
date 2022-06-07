a91c1c0383e4f4f63a3ac09d00a223b3e05ce17a
Wandeir Carneiro de Souza <domjesus@gmail.com>
2022-06-07 16:31:19 -0300

    Ajuste no connect db

--- database/db.go
+++ database/db.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"os"
+
 	"github.com/domjesus/api-go-gin/models"
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
@@ -15,9 +17,9 @@ var (
 func ConectaComBancoDeDados(l *zap.SugaredLogger) error {
 
 	// stringDeConexao := os.Getenv("DATABASE_URL")
-	// stringDeConexao := "host=" + os.Getenv("DATABASE_HOST") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PASSWORD") + " dbname=" + os.Getenv("DATABASE_NAME") + " port=5432 sslmode=require"
+	stringDeConexao := "host=" + os.Getenv("DATABASE_HOST") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PASSWORD") + " dbname=" + os.Getenv("DATABASE_NAME") + " port=5432 sslmode=require"
 
-	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	// stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		return err
