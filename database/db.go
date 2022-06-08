3a040c2905d21e95ebc857218426b3f0959536a6
Wandeir Carneiro de Souza <domjesus@gmail.com>
2022-06-08 15:27:39 -0300

    Alt db

--- database/db.go
+++ database/db.go
@@ -1,11 +1,9 @@
 package database
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/domjesus/api-go-gin/models"
-	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -19,20 +17,19 @@ var (
 func ConectaComBancoDeDados(l *zap.SugaredLogger) error {
 
 	// stringDeConexao := os.Getenv("DATABASE_URL")
-	err := godotenv.Load()
-	if err != nil {
-		l.Error("Error loading .env file")
-	}
-	env := os.Getenv("ENV")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	l.Error("Error loading .env file")
+	// }
+	// env := os.Getenv("ENV")
 
-	var stringDeConexao string
-	fmt.Println("Ambiente: ", env)
+	// var stringDeConexao string
+	// fmt.Println("Ambiente: ", env)
 
-	if env != "local" {
-		stringDeConexao = "host=" + os.Getenv("DATABASE_HOST") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PASSWORD") + " dbname=" + os.Getenv("DATABASE_NAME") + " port=5432 sslmode=require"
-	} else {
-		stringDeConexao = "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
-	}
+	// if env != "local" {
+	stringDeConexao := "host=" + os.Getenv("DATABASE_HOST") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PASSWORD") + " dbname=" + os.Getenv("DATABASE_NAME") + " port=5432 sslmode=require"
+
+	// stringDeConexao = "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
